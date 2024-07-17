0ccdba377f008ab4c67bb2539ad136d59c6d88ea
xg203 <xingong98@gmail.com>
2024-07-17 13:46:59 +0000

    u

--- gnomad_mt_anno_test.py
+++ gnomad_mt_anno_test.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('./gnomad_qc')
-sys.path.append('./gnomad_mitochondria')
+sys.path.append('./gnomad-mitochondria')
 import hail as hl
 from gnomad_mitochondria.utils.annotations import apply_indel_stack_filter 
 hl.init()
